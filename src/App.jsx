3f76070816c58b1a0b498506920dd94e6102b642
Azizan <zhiz.nevermind@gmail.com>
2025-03-02 13:23:19 +0700

    testing taildwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="bg-blue-600">
-      <h1 className="text-1xl underline">Hello world!</h1>
+    <div className="bg-blue-300 place-content-center min-h-screen">
+      <h1 className="text-3xl underline text-center">Hello world!</h1>
     </div>
   );
 }
