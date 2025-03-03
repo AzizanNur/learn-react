f7b15b4021989afba45f5eaccef0a28e8ca4aa2b
Azizan <zhiz.nevermind@gmail.com>
2025-03-03 13:27:11 +0700

    simple componen untuk button dan props children

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 function App() {
   return (
     <div className="bg-slate-900 place-content-center grid min-h-screen">
-      <button className="bg-blue-600 text-white px-4 py-2 rounded">
-        Sign up
-      </button>
+      <div className="flex gap-x-2">
+        <Button>Register</Button>
+        <Button>Sign In</Button>
+      </div>
     </div>
+  );
+}
+
+function Button(props) {
+  const { children } = props;
+  return (
+    <button className="bg-blue-600 text-white px-4 py-2 rounded">
+      {children}
+    </button>
   );
 }
 
